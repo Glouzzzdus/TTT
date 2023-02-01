574fdaa82d12559390e67568875afe6437e2e023
Glouzzzdus <glouzzzdus@gmail.com>
2023-02-01 23:59:37 +0200

    Let's see

---
+++ index.js
@@ -0,0 +1,62 @@
+function changeClass(el){
+    el.classList.toggle("ring");
+    el.classList.toggle("cross");
+}
+
+function refresh(){
+    let randomImage = Math.floor(Math.random()* 10);    
+    let c1 = document.getElementById("player");
+    if(randomImage%2===0 & c1.className === "ring"){        
+        c1.classList.replace("ring", "cross");        
+    }
+    else {  
+        c1.classList.replace("cross", "ring");   
+    }
+    var divs = document.querySelectorAll("td > div");
+    for (let el of divs) {
+        if(el.className === 'ring' || el.className === 'cross'){
+            el.className = ''
+        }
+    }    
+}
+
+function isVictory(fields) {
+	let combinations = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let comb of combinations) {
+		if (
+			fields[comb[0]].className === fields[comb[1]].className &&
+			fields[comb[1]].className === fields[comb[2]].className &&
+			fields[comb[0]].className !== ""
+		) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function putSign(el){
+    let fields = document.querySelectorAll("td > div");
+    let c1 = document.getElementById("player");
+    if(el.querySelector("div").className === ""){
+        el.querySelector("div").className = c1.className;        
+        if (isVictory(fields)){
+            console.log(c1.className);
+            alert(this.textContent);
+        }
+        changeClass(c1);        
+    }    
+}
+
+
+
