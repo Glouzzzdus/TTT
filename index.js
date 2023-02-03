082df0f17e38fe609fd3ec0d722cec45d4b1b27a
Glouzzzdus <glouzzzdus@gmail.com>
2023-02-03 11:39:36 +0200

    comments and refactor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let isDone = false;
+let isGameOver = false; //The flag of Game Over.
+let fields = document.querySelectorAll("td > div"); //Game cells.
 
 function changeClass(el){
     el.classList.toggle("ring");
@@ -6,10 +7,12 @@ function changeClass(el){
 }
 
 function refresh(){
-    isDone = false;
+    isGameOver = false;
+    //Restore table header after Game Over.
     document.getElementById("player-title").className = "choise-player";
     document.getElementById("player-sign").className = "choise-player";
     document.getElementById("player-title").textContent = "Active player";
+    //Generate first player.
     let randomImage = Math.floor(Math.random()* 10);    
     let c1 = document.getElementById("player");
     if(randomImage%2===0 & c1.className === "ring"){        
@@ -18,14 +21,14 @@ function refresh(){
     else {  
         c1.classList.replace("cross", "ring");   
     }
-    var divs = document.querySelectorAll("td > div");
-    for (let el of divs) {
-        if(el.className === 'ring' || el.className === 'cross'){
-            el.className = ''
+    //Cleaning game field.
+    for (let field of fields) {
+        if(field.className === "ring" || field.className === "cross"){
+            field.className = "empty";
         }
     }    
 }
-
+//Checking for a winner.
 function isVictory(fields) {
 	let combinations = [
 		[0, 1, 2],
@@ -42,51 +45,56 @@ function isVictory(fields) {
 		if (
 			fields[comb[0]].className === fields[comb[1]].className &&
 			fields[comb[1]].className === fields[comb[2]].className &&
-			fields[comb[0]].className !== ""
+			fields[comb[0]].className !== "empty"
 		) {
 			return true;
 		}
 	}
 	return false;
 }
-
-function IsDraw(fields){
+//Checkin for a draw.
+function isDraw(fields){    
     for (let field in fields){
-        if (field.className === ""){
+        if (field.className === "empty"){
             return false;
         }        
     }
     return true;
 }
 
-function putSign(el){
-    if(isDone){
+function putSign(el){    
+    //Block clicking after win.
+    if(isGameOver){
+        return;
+    }    
+    let c1 = document.getElementById("player");//Active player.
+    if(el.querySelector("div").className === "empty"){
+        el.querySelector("div").className = c1.className; //This turn.
+    }     
+    //Checking for a winner.
+    if (isVictory(fields)){
+        isGameOver = true;
+        if(c1.className === "cross"){
+            document.getElementById("player-title").classList.add("winner-cross");
+            document.getElementById("player-sign").classList.add("winner-cross");
+            document.getElementById("player-title").textContent = "Cross won!!!";                                
+        }
+        else if(c1.className === "ring"){
+            document.getElementById("player-title").className = "winner-rings";
+            document.getElementById("player-sign").className = "winner-rings";
+            document.getElementById("player-title").textContent = "Rings won!!!";
+        }
+        return;          
+    }
+    //Checking for a draw.
+    if (!isDraw(fields)){
+        isGameOver = true;
+        document.getElementById("player-title").textContent = "- DRAW -";
+        console.log("draw")
         return;
     }
-    let fields = document.querySelectorAll("td > div");
-    let c1 = document.getElementById("player");
-    if(el.querySelector("div").className === ""){
-        el.querySelector("div").className = c1.className;        
-        if (isVictory(fields)){
-            isDone = true;
-            if(c1.className === "cross"){
-                document.getElementById("player-title").classList.add("winner-cross");
-                document.getElementById("player-sign").classList.add("winner-cross");
-                document.getElementById("player-title").textContent = "Cross won!!!";                                
-            }
-            else if(c1.className === "ring"){
-                document.getElementById("player-title").className = "winner-ring";
-                document.getElementById("player-sign").className = "winner-ring";
-                document.getElementById("player-title").textContent = "Rings won!!!";
-            }
-            return;          
-        }
-        if (!IsDraw(fields)){
-            document.getElementById("player-title").textContent = "- DRAW -";
-            return;
-        }
-        changeClass(c1);        
-    }    
+    //Changing active player.
+    changeClass(c1);     
 }
 
 
